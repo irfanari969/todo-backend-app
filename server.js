92ea13160b7867401db0f0cdd3036caa309cf6a6
irfanari969 <irfanari969@gmail.com>
2026-03-01 17:09:36 +0700

    initial project

---
+++ server.js
@@ -0,0 +1,91 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const mysql = require('mysql2')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server berjalan di port ${port}`);
+})
+
+
+app.use(cors())
+app.use(express.json())
+
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,     
+    user: process.env.DB_USER,    
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME   
+})
+
+db.connect(err => {
+    if (err) console.log('Error koneksi DB:', err)
+    else console.log('Terkoneksi ke MySQL!')
+})
+
+// --- ROUTES (API) ---
+
+app.get('/tasks', (req, res) => {
+    const sql = 'SELECT * FROM tasks ORDER BY created_at DESC'
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json(err)
+        res.json(results);
+    })
+})
+
+
+app.post('/tasks', (req, res) => {
+    const { task_name, due_date, priority } = req.body
+    const sql = 'INSERT INTO tasks (task_name, due_date, priority) VALUES (?, ?, ?)'
+    
+    db.query(sql, [task_name, due_date, priority], (err, result) => {
+        if (err) return res.status(500).json(err)
+        res.status(201).json({ message: 'Task created', id: result.insertId })
+    })
+})
+
+
+app.patch('/tasks/:id/status', (req, res) => {
+    const { id } = req.params
+    const { completed } = req.body
+    
+    const sql = 'UPDATE tasks SET completed = ? WHERE id = ?'
+    
+    db.query(sql, [completed, id], (err, result) => {
+        if (err) return res.status(500).json(err)
+        res.json({ message: 'Status updated' })
+    })
+})
+
+app.delete('/tasks/bulk/completed', (req, res) => {
+    const sql = 'DELETE FROM tasks WHERE completed = 1'
+    db.query(sql, (err, result) => {
+        if (err) return res.status(500).json(err)
+        res.json({ message: 'Completed tasks deleted' })
+    })
+})
+
+app.delete('/tasks/bulk/all', (req, res) => {
+    const sql = 'DELETE FROM tasks'
+    db.query(sql, (err, result) => {
+        if (err) return res.status(500).json(err)
+        res.json({ message: 'All tasks deleted' })
+    })
+})
+
+app.delete('/tasks/:id', (req, res) => {
+    const { id } = req.params
+    const sql = 'DELETE FROM tasks WHERE id = ?'
+    db.query(sql, [id], (err, result) => {
+        if (err) return res.status(500).json(err)
+        res.json({ message: 'Task deleted' })
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`)
+})
---
+++ sql/todo_db.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Nov 2025 pada 11.53
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `todo_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `task_name` varchar(255) NOT NULL,
+  `completed` tinyint(1) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `due_date` date DEFAULT NULL,
+  `priority` enum('low','medium','high') DEFAULT 'medium'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `task_name`, `completed`, `created_at`, `due_date`, `priority`) VALUES
+(1, 'Belajar Integration', 0, '2025-11-24 09:30:28', NULL, 'medium');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
